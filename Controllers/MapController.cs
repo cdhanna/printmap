7cff6bdfceee6f1eda09f56ece813665a160585f
Chris Hanna <cdhannaphone@gmail.com>
2018-04-16 16:26:51 -0400

    #changes sample valuesController to mapController

--- Controllers/MapController.cs
+++ Controllers/MapController.cs
@@ -9,16 +9,8 @@ using printmap.Services;
 namespace printmap.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class MapController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
@@ -35,25 +27,6 @@ namespace printmap.Controllers
 
             return $"data:image/png;base64,{SigBase64}";
 
-        }
-
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
