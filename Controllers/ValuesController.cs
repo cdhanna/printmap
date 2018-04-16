21018981e0ba9cf705fbb686a93c7d3aa482573a
Chris Hanna <cdhannaphone@gmail.com>
2018-04-16 13:48:21 -0400

    #adds repo. #adds basic mapbox http

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using printmap.Services;
+
+namespace printmap.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            var map = new MapDataService(); // todo, add unity container or some such ingest
+            var imageTask = map.GetBitmapForRegion();
+            imageTask.Wait();
+
+            var image = imageTask.Result;
+
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] byteImage = ms.ToArray();
+            var SigBase64= Convert.ToBase64String(byteImage); //Get Base64
+
+            return $"data:image/png;base64,{SigBase64}";
+
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Services/MapDataService.cs
@@ -0,0 +1,40 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace printmap.Services 
+{
+
+
+    public class MapDataService 
+    {
+
+        private string _token = "pk.eyJ1IjoiY2RoYW5uYSIsImEiOiJjaXVoY3AxdGUwMHVmM3BxZnZnMGFtZDA0In0.G2Gjh4DkpoyC1h9nZFaL5g"; // todo make a config option
+
+        public async Task<Bitmap> GetBitmapForRegion(){
+            //"https://api.mapbox.com/v4/mapbox.streets/1/0/0.png?access_token=your-access-token"
+
+            var url = BuildUrl();
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.GetAsync(url))
+                using (HttpContent content = res.Content)
+                {
+                    res.EnsureSuccessStatusCode();
+
+                    var stream = await content.ReadAsStreamAsync();
+
+                    var image = System.Drawing.Image.FromStream(stream);
+                    return new Bitmap(image);
+                }
+            }
+
+        }
+
+        private string BuildUrl(){
+            return $"https://api.mapbox.com/v4/mapbox.streets/{1}/{0}/{0}.png?access_token={_token}"; // todo pull options out into config options
+        }
+
+    }
+
+}
