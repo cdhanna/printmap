81daa949e7ef716e8e97594200b8e010f3af065d
Chris Hanna <cdhannaphone@gmail.com>
2018-04-16 16:43:25 -0400

    #adds get tile

--- Services/MapDataService.cs
+++ Services/MapDataService.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Drawing;
+using System;
 
 namespace printmap.Services 
 {
@@ -30,6 +31,21 @@ namespace printmap.Services
             }
 
         }
+
+        private TileCoord GetTile(float lat, float lon, int zoom=16){
+            /*
+            from https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+            n = 2 ^ zoom
+            xtile = n * ((lon_deg + 180) / 360)
+            ytile = n * (1 - (log(tan(lat_rad) + sec(lat_rad)) / π)) / 2
+             */
+            var n = Math.Pow(2, zoom);
+            var x = n * ((lon + 180) / 360);
+            var lat_rad = lat * Math.PI / 180;
+            var y = (1 - (Math.Log( Math.Tan(lat_rad) + Math.Asin(lat_rad)) / Math.PI)) / 2;
+            return new TileCoord((int)x, (int)y, zoom);
+        }
+
 
         private string BuildUrl(){
             return $"https://api.mapbox.com/v4/mapbox.streets/{1}/{0}/{0}.png?access_token={_token}"; // todo pull options out into config options
